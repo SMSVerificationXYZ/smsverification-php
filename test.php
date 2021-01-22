c5b8e1eed3dac7d4457a4138e281458b126419e1
tamir <tamirfar23@gmail.com>
2021-01-22 16:51:55 +0200

    composer init

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-use SMSVerification\SMSVerification;
-
-require_once "SMSVerification.php";
-
-$api = new SMSVerification("user","pass");
-echo $api->getUserActions()->getBalance(); // getting the current balance and printing it.
